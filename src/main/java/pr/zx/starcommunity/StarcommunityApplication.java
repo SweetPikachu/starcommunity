c51a4e2976961c75ca856af1f7fcdfcc22e3cf0a
zstar <892599718@qq.com>
2021-07-18 15:26:06 +0800

    add README

--- src/main/java/pr/zx/starcommunity/StarcommunityApplication.java
+++ src/main/java/pr/zx/starcommunity/StarcommunityApplication.java
@@ -1,7 +1,7 @@
 package pr.zx.starcommunity;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StarcommunityApplication {
